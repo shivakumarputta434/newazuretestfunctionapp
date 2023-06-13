a7e0c9dc5db79ae7980a46dfc8334e8ae3f2fc95
shivakumar <shivakumar.putta434@gmail.com>
2023-06-13 19:27:55 +0530

    second  commit

--- testfunction/__init__.py
+++ testfunction/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    data = {"name":"myra", "age":10}
+    data = {"name":"myra", "age":10, "village":"peddampet"}
     return func.HttpResponse(
             json.dumps(data),
             status_code=200
